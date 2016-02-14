aaa295184c9c090b1a284c34e831fb0feebf84ae
rhadley <rhadley@microsoft.com>
2016-02-14 15:30:49 -0800

    Initial base/diff functionality working.  Disasm'd assemblies put in flat directory.  Still needs more work to clean up output.

--- mcgdiff.cs
+++ mcgdiff.cs
@@ -4,58 +4,89 @@ using System.CommandLine;
 using System.IO;
 using System.Collections.Generic;
 using Microsoft.DotNet.Cli.Utils;
+using Microsoft.DotNet.Tools.Common;
 
 namespace ManagedCodeGen
 {
     // Define options to be parsed 
-    class Config
+    public class Config
     {
-        public bool genFrameworkAssemblies { get { return true; } }
-        public string assemblyPath { get { return "assemblyPath"; } }
+        private bool genFrameworkAssemblies = true;
+        private string baseExe = "crossgen.exe";
+        private string diffExe = null;
+        private string rootPath = null;
+        private IReadOnlyList<string> assemblyList = Array.Empty<string>();
+        private bool wait = false;
+        private bool recursive = false;
+        private IReadOnlyList<string> methods = Array.Empty<string>();
+        
+        public Config(string[] args) {
+            // App name is currently not derived correctly due to some API issues - this
+            // will be fixed in the future.
+            var result = ArgumentSyntax.Parse(args, syntax =>
+            {
+                syntax.DefineOption("f|frameworks", ref genFrameworkAssemblies, "Generate asm for default framework assemblies");
+                syntax.DefineOption("b|base", ref baseExe, "The base crossgen exe.");
+                syntax.DefineOption("d|diff", ref diffExe,  "The diff crossgen exe.");
+                syntax.DefineOption("o|output", ref rootPath, "The output path.");
+                syntax.DefineOption("w|wait", ref wait, "Wait for debugger to attach.");
+                syntax.DefineOption("r|recursive", ref recursive, "Scan directories recursivly.");
+                syntax.DefineOptionList("m|methods", ref methods, "List of methods to disasm.");
+                
+                syntax.DefineParameterList("assembly", ref assemblyList, "The list of assemblies or directories to scan for assemblies."); 
+            });
+            
+            System.Console.WriteLine("Parsing commandline:");
 
-        //[Option('b', "base", Required = false, HelpText = "Base code generator")]
-        public string baseExe { get { return "basePath"; } }
-        //[Option('d', "diff", Required = false, HelpText = "Diff code generator")]
-        public string diffExe { get { return "diffPath"; } }
-        //[Option('o', "output", Required = true, HelpText = "Output Directory")]
-        public string outputPath { get { return "outputPath"; } }
+            System.Console.WriteLine("base: {0}", baseExe);
+            System.Console.WriteLine("diff: {0}", diffExe);
+            System.Console.WriteLine("rootPath: {0}", rootPath);
+            
+            // Weird, ToArray not working on IReadOnlyList in core.
+            foreach(var assembly in assemblyList) {
+                System.Console.WriteLine("assembly: {0}", assembly);
+            }
+        }
+        
+        public bool GenFrameworkAssemblies { get { return genFrameworkAssemblies; }}
+        public bool GenUserAssemblies { get { return AssemblyList.Count > 0; }}
+        public bool WaitForDebugger { get { return wait; }}
+        public bool GenerateBaseline { get { return (baseExe != null); }}
+        public bool GenerateDiff { get { return (diffExe != null); }}
+        public bool Recursive { get { return recursive; }}
+        public string BaseExecutable { get { return baseExe; }}
+        public string DiffExecutable { get { return diffExe; }}
+        public string RootPath { get { return rootPath; }}
+        public IReadOnlyList<string> AssemblyList { get { return assemblyList; }}
     }
 
-    public class MCGDiff
+    class AssemblyInfo {
+        public string Name {get; set;}
+        // Contains path to assembly.
+        public string Path {get; set;}
+        // Contains relative path within output directory for given assembly.
+        // This allows for different output directories per tool.
+        public string OutputPath {get; set;}
+    }
+
+    public class mcgdiff
     {
         public static void Main(string[] args)
         {
-            var baseExe = "";
-            var diffExe = "";
-            var outputPath = "d:\\\\output\\";
-            var assemblyName = "";
-            
-            // App name is currently not derived correctly due to some API issues - this
-            // will be fixed in the future.
-            ArgumentSyntax.Parse(args, syntax =>
-            {
-                syntax.DefineOption("b|base", ref baseExe, "The base crossgen exe");
-                syntax.DefineOption("d|diff", ref diffExe,  "The diff crossgen exe");
-                syntax.DefineOption("o|output", ref outputPath, "The output path");
-                
-                syntax.DefineParameter("assembly", ref assemblyName, "The assembly to asm diff"); 
-            });
-            
-            System.Console.WriteLine("mcgdiff");
+            // Parse and store comand line options.
+            var config = new Config(args);
 
             // Stop to attach a debugger if desired.
-            //WaitForDebugger();
-
-            var config = new Config();
-
-            // set config values based on arguments.
-
-            if (config != null)
-            {
-                DifferenceEngine diff = new DifferenceEngine(config);
-                diff.GenerateAssemblyWorklist();
-                diff.Execute();
+            if (config.WaitForDebugger) {
+                WaitForDebugger();
             }
+
+            // The difference engine encapsulates a particular set of diffs.  An engine is
+            // produced with a given config, which then lets it generate a particular worklist
+            // after which it executes the diff.
+            DifferenceEngine diff = new DifferenceEngine(config);
+            diff.GenerateAssemblyWorklist();
+            diff.Execute();
         }
 
         private static void WaitForDebugger() {
@@ -70,7 +101,6 @@ namespace ManagedCodeGen
             
             private static string baseExecutablePath = "D:\\dotnet\\coreclr\bin\\Product\\Windows_NT.x64.Debug";
             private static string diffExecutablePath;
-            private static string outputPath = "D:\\output";
 
             private bool generateBase = true;
             private bool generateDiff = false;
@@ -82,7 +112,9 @@ namespace ManagedCodeGen
             // TODO: Does crossgen also require these restrictions??
             private static string[] frameworkAssemblies =
             {
-            "mscorlib",
+            "mscorlib.dll"
+#if false
+            ,
             "System",
             "System.Core",
             "System.Configuration",
@@ -135,9 +167,10 @@ namespace ManagedCodeGen
             "System.Xaml.Hosting",
             "System.Data.Linq",
             "System.ComponentModel.DataAnnotations"
+#endif
             };
 
-            List<string> assemblyList = null;
+            List<AssemblyInfo> assemblyInfoList = null;
 
             public DifferenceEngine(Config config)
             {
@@ -146,51 +179,134 @@ namespace ManagedCodeGen
 
             public void Execute()
             {
-                if (generateBase)
-                {
-                    GenerateAsm(baseExecutablePath, assemblyList, outputPath);
+                if (config.GenerateBaseline) {
+                    var baselinePath = "base";
+                    
+                    if (config.RootPath != null) {
+                        baselinePath = Path.Combine(config.RootPath, baselinePath);
+                    }
+
+                    // Traverse the assembly list and generate asm into the root directory
+                    // taged to make it unique.
+                    GenerateAsm(config.BaseExecutable, baselinePath, assemblyInfoList);
                 }
 
-                if (generateDiff)
-                {
-                    GenerateAsm(diffExecutablePath, assemblyList, outputPath);
+                if (config.GenerateDiff) {
+                    var diffPath = "diff";
+                    
+                    if (config.RootPath != null) {
+                        diffPath = Path.Combine(config.RootPath, diffPath);
+                    }
+
+                    GenerateAsm(config.DiffExecutable, diffPath, assemblyInfoList);
                 }
             }
 
             public void GenerateAssemblyWorklist()
-            {
-                if (config.genFrameworkAssemblies) {
+            {      
+                if (config.GenFrameworkAssemblies) {
+                    // TODO get a path to a scratch project that pulls down the full list.  
+                    // For now we just will use mscorlib.
+                    var basePath = Path.GetDirectoryName(config.BaseExecutable);
                     // build list based on baked in list of assemblies
-                    assemblyList = new List<string>(frameworkAssemblies);  
+                    assemblyInfoList = new List<AssemblyInfo>();
+                    
+                    foreach (var assembly in frameworkAssemblies) {
+                        // find assembly path, and compute output path.
+                        AssemblyInfo info = new AssemblyInfo {
+                            Name = assembly,
+                            Path = basePath,
+                            OutputPath =  ""
+                        };
+                        
+                        assemblyInfoList.Add(info);
+                    }  
                 }
+
+                if (config.GenUserAssemblies) {
+                    var assemblyList = config.AssemblyList;
+                    
+                    foreach (var path in assemblyList)
+                    {
+                        FileAttributes attr = File.GetAttributes(path);
+                        
+                        if ((attr & FileAttributes.Directory) == FileAttributes.Directory) {
+                            // This is directory case.
+                            System.Console.WriteLine("NYI - directory case");
+                            Environment.Exit(-1);
+                        }
+                        else {
+                            // This is the file case.
+                            
+                            System.Console.WriteLine("File case.");
+
+                            AssemblyInfo info = new AssemblyInfo {
+                            Name = Path.GetFileName(path),
+                            Path = Path.GetDirectoryName(path),
+                            OutputPath = ""
+                            };
+                        }
+                    }
+                }
+                
+                // case for single assembly
             }
 
-            public void GenerateAsm(string codegenExe, List<string> assemblies, string outputPath)
+            public void GenerateAsm(string codegenExe, string rootPath, List<AssemblyInfo> infoList)
             {   
-                // Build a command per assembly to generate the asm output
-              
-                foreach(var assembly in assemblies) {
-                    string fullpathAssembly = "D:\\dotnet\\coreclr\\bin\\Product\\Windows_NT.x64.Debug\\" + assembly + ".dll";
+                // Build a command per assembly to generate the asm output.
+                foreach (var assembly in infoList)
+                {
+                    string fullpathAssembly = Path.Combine(assembly.Path, assembly.Name);
                     List<string> commandArgs = new List<string>() {fullpathAssembly};
-
+                    
+                    Console.WriteLine("args: {0}", string.Join(" ", commandArgs.ToArray()));
+                    
                     Command generateCmd = Command.Create(
-                        "D:\\dotnet\\coreclr\\bin\\Product\\Windows_NT.x64.Debug\\crossgen.exe", 
+                        codegenExe, 
                         commandArgs);
 
-                    // Generate stream writer for the output file
-                    
-                    StreamWriter outputFile = new StreamWriter(outputPath + assembly + ".asm");
-                    
-                    generateCmd.ForwardStdOut(outputFile);
-                    generateCmd.ForwardStdErr(outputFile);
-
+                    // Set up environment do disasm.
                     generateCmd.EnvironmentVariable("COMPlus_NgenDisasm", "*");
 
-                    generateCmd.Execute();
-                
-                    Console.WriteLine("Finished processing {0}.", assembly);
+                    if (config.RootPath != null) {
+                        // Generate path to the output file
+                        var assemblyFileName = Path.ChangeExtension(assembly.Name, ".dasm");
+                        var path = Path.Combine(rootPath, assembly.OutputPath, assemblyFileName);
+                        
+                        PathUtility.EnsureParentDirectory(path);
+                        
+                        Console.WriteLine("Generating to {0}", path);
+                        // Redirect stdout/stderr to disasm file and run command.
+                        using (var outputStream = System.IO.File.Create(path)) {
+                            using (var outputStreamWriter = new StreamWriter(outputStream)) {
+                                
+                                // Forward output and error to file.
+                                generateCmd.ForwardStdOut(outputStreamWriter);
+                                generateCmd.ForwardStdErr(outputStreamWriter);
+
+                                Console.WriteLine("Starting processing {0}.", assembly.Name);
+
+                                generateCmd.Execute();
+                        
+                                Console.WriteLine("Finished processing {0}.", assembly.Name);
+                            }
+                        }
+                    }
+                    else {
+
+                        // By default forward to output to stdout/stderr.
+                        generateCmd.ForwardStdOut();
+                        generateCmd.ForwardStdErr();
+                        
+                        Console.WriteLine("Starting processing {0}.", assembly.Name);
+
+                        generateCmd.Execute();
+                        
+                        Console.WriteLine("Finished processing {0}.", assembly.Name);
+                    }
                 }
-            }
+            }   
         }
     }
 }
